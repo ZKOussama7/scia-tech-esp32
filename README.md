6757e337b3c767c83b3b6421bda13a2f07122471
ZKOussama7 <zaki.oussama.aiac@gmail.com>
2023-09-14 12:57:11 +0100

    Init Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sciatech Insole Esp32 Code
+The esp32 code to gather oriotation and pressure data from sensors, and calculate speed accordingly.
---
+++ Sciatech_ESP/Sciatech_ESP.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sciatech_ESP.ino
+ Created:	8/8/2023 2:28:20 PM
+ Author:	ODC_SSS
+*/
+not required anymore.
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
